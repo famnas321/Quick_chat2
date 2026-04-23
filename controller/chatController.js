0807a69064085a03b4e6746500791df3b0d1f199
famnas321 <famnaspk380@gmail.com>
2026-04-23 16:37:37 +0530

    committed on 23.4.2026 /3

--- controller/chatController.js
+++ controller/chatController.js
@@ -10,3 +10,18 @@ exports.createNewChat = async (req,res)=>{
        res.status(500).json({error:"error occured while new chat creating ", error})
      }
 }
+exports.fetchAllChats= async (req,res)=>{
+        const id= req.userId
+     try{
+         
+          console.log(id,"this is from chat controller")
+          const allChats= await  chats.find({members:{$in:id}})
+
+          res.status(200).json({message:"chats fetches successfully",allChats})
+          console.log(allChats)
+     }catch(error){
+          res.status(500).json({errorMessage: "this error occured while fetching all chats",error})
+          console.log("error while fetching chats",error)
+     }
+
+}
--- router/chatRouter.js
+++ router/chatRouter.js
@@ -1,6 +1,8 @@
 const router  = require("express").Router()
-// const {authMiddleware}= require("../middleware/authMiddleware")
-const {createNewChat} = require("../controller/chatController")
-router.post("/create-new-chat", createNewChat)
+ const authMiddleware= require("../middleware/authMiddleware")
+const {createNewChat,fetchAllChats} = require("../controller/chatController")
+router.post("/create-new-chat",createNewChat)
+router.get("/fetchChats", authMiddleware,fetchAllChats)
+
 
 module.exports=router
